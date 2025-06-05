874d651450913afae0517b7cb00231ddeee11d7d
anikasikka <anikasikka@berkeley.edu>
2025-06-05 14:24:32 -0700

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
           height={120}
           priority
         />
-        <Imagea
+        <Image
           className="dark:invert"
           src="/210617-austin-stock.jpg"
           alt="Next.js logo"
