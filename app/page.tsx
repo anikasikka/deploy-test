846cc8acc1ffee0ed23aa1cf32bebfdd740029c7
anikasikka <anika.sikka@vercel.com>
2025-07-07 09:10:37 -0700

    asfasda

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <h2 
         id="title" 
         style={{ textAlign: 'center', padding: 30, color: 'white'}}
-      ><b>Some cool photos! change to see if legacy states get triggered #haha what why isn't this  changes to get queued hahahahaha</b></h2>
+      ><b>Some cool photos! change to see if legacy states get triggered #haha what why isn't serserserserser to get queued hahahahaha</b></h2>
 
       <div style={{ display: 'flex', justifyContent: 'center', padding: '50px 0', gap:20 }}>
         <Image
