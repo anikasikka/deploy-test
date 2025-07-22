d84c8c17711507c8e160c5f392a16a27f3b7e90d
anikasikka <anika.sikka@vercel.com>
2025-07-22 15:30:02 -0700

    anika

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import Image from "next/image";
 
-const requiredVar = process.env.NEXT_PUBLIC_REQUIRED_VAR;
-if (!requiredVar) {
-  throw new Error("NEXT_PUBLIC_REQUIRED_VAR is not set");
-}
 
 export default function Home() {
   return (
@@ -11,7 +7,7 @@ export default function Home() {
       <h2 
         id="title" 
         style={{ textAlign: 'center', padding: 30, color: 'white'}}
-      ><b>Some cool photos! hello world</b></h2>
+      ><b>Some cool photos! hello world oops maybe something is not queued now</b></h2>
 
       <div style={{ display: 'flex', justifyContent: 'center', padding: '50px 0', gap:20 }}>
         <Image
